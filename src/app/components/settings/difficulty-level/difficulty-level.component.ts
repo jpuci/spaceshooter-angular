975dea54d511f32f69a73bc208823263d205c63b
Joanna Puciłowska <jpucilowska@tlen.pl>
2024-02-13 22:13:11 +0100

    add difficulty level settings

---
+++ src/app/components/settings/difficulty-level/difficulty-level.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-difficulty-level',
+  templateUrl: './difficulty-level.component.html',
+  styleUrls: ['./difficulty-level.component.scss']
+})
+export class DifficultyLevelComponent {
+
+}
