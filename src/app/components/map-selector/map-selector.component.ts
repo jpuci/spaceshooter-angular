78b0d77d852988faf5b5b6cb9f7ca7e6e0d5fc5c
kwachala <dragonik@onet.pl>
2024-03-06 18:04:05 +0100

    map selector done

--- src/app/components/map-selector/map-selector.component.ts
+++ src/app/components/map-selector/map-selector.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-map-selector',
   templateUrl: './map-selector.component.html',
   styleUrls: ['./map-selector.component.scss']
 })
-export class MapSelectorComponent {
+export class MapSelectorComponent implements OnInit {
+  maps: string[] = ['assets/map1.jpg', 'assets/map2.jpg', 'assets/map3.jpg'];
+  selectedMapIndex: number = 1;
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  selectMap(index: number): void {
+    this.selectedMapIndex = index;
+  }
+
+  navigateLeft(): void {
+    this.selectedMapIndex = (this.selectedMapIndex - 1 + this.maps.length) % this.maps.length;
+  }
+
+  navigateRight(): void {
+    this.selectedMapIndex = (this.selectedMapIndex + 1) % this.maps.length;
+  }
 }
