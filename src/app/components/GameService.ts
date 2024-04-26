2f59abe691602ec11e5e81b98654aad6296a95fb
252880 <tombrys11@gmail.com>
2024-04-26 20:02:30 +0200

    Save to file

--- src/app/components/GameService.ts
+++ src/app/components/GameService.ts
@@ -145,7 +145,7 @@ export class GameService {
       speed_4: 1,
       speed: 1,
       selectedJet: '1',
-      coins: 0,
+      coins: 30,
       bestscore: 0,
       bullets: 1,
       points: '',
